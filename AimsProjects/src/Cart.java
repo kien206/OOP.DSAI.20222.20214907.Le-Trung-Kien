eb8d29e266acb2c7b7b93c9c99ed26f72f7b6cb6
Trung Kien <ltrkien2062003@gmail.com>
2023-05-27 10:59:00 +0700

    Method overloading

--- AimsProjects/src/Cart.java
+++ AimsProjects/src/Cart.java
@@ -1,7 +1,7 @@
 public class Cart {
     public static final int MAX_NUMBERS_ORDERED = 20;
     private DigitalVideoDisc itemsOrdered[] = new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
-    private int qtyOrderd = 0;
+    protected int qtyOrderd = 0;
     
     public void addDigitalVideoDisc (DigitalVideoDisc disc) {
         for (int i=0; i<MAX_NUMBERS_ORDERED; i++) {
@@ -14,6 +14,33 @@ public class Cart {
             if ((i == itemsOrdered.length -1) && (itemsOrdered[i] != null)) {
                 System.out.println("The cart is full.");
                 break;
+            }
+        }
+    }
+
+    public void addDigitalVideoDisc(DigitalVideoDisc [] dvdList) {
+
+        for (DigitalVideoDisc dvd : dvdList) {
+            for (int i=0; i<MAX_NUMBERS_ORDERED; i++) {
+                if (itemsOrdered[i] == null) {
+                    itemsOrdered[i] = dvd;
+                    qtyOrderd += 1;
+                    System.out.println("The disc has been added.");
+                }
+            }
+        }
+    }
+
+    public void addDigitalVideoDisc(DigitalVideoDisc dvd1, DigitalVideoDisc dvd2) {
+        for (int i=0; i<MAX_NUMBERS_ORDERED; i++) {
+            if (itemsOrdered[i] == null) {
+                itemsOrdered[i] = dvd1;
+                if ((i+1) <= MAX_NUMBERS_ORDERED) {
+                    itemsOrdered[i+1] = dvd2;    
+                }
+                else {
+                    System.out.println("The cart is full.");
+                }
             }
         }
     }
